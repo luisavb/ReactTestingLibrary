9d858b3667cd576896057c6c838751f9ad18b2b0
Luísa Vilas Boas dos Santos <luisa13santos@outlook.com>
2022-05-09 17:34:53 -0300

    Requisito 4-teste do componente NotFound

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste componente FavoritePokemon.js', () => {
+  it('Teste se é exibida "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+//   it('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+//     renderWithRouter(<FavoritePokemons />);
+//     const favorites = screen.getByTestId('pokemon-name');
+//     expect(favorites).toBeInTheDocument();
+//   });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente NotFound.js', () => {
+  it('Teste se a página contém um h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+    const notFound = screen
+      .getByRole('heading', { level: 2, name: 'Page requested not found Crying emoji' });
+    expect(notFound).toBeInTheDocument();
+  });
+  it('Teste se a página mostra a imagem do Pikachu chorando', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundImg = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(notFoundImg.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+//   it('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+//     renderWithRouter(<FavoritePokemons />);
+//     const favorites = screen.getByTestId('pokemon-name');
+//     expect(favorites).toBeInTheDocument();
+//   });
+});
