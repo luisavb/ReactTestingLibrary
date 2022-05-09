8a1515a576a7267b15e5f3d16125fe586fce585e
Luísa Vilas Boas dos Santos <luisa13santos@outlook.com>
2022-05-09 16:28:35 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Teste componente About.js', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    renderWithRouter(<About />);
+
+    const aboutTitle = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const aboutTextOne = screen
+      .queryByText(/One can filter Pokémons /i);
+    const aboutTextTwo = screen
+      .getByText(/This application simulates/i);
+    expect(aboutTextOne).toBeInTheDocument();
+    expect(aboutTextTwo).toBeInTheDocument();
+  });
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const img = screen.getByRole('img');
+    expect(img.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
